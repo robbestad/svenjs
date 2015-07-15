b6350342b2cdf9fd43abb95c1bb8b0cb50ed4348
Sven Anders Robbestad <robbestad@gmail.com>
2015-07-15 10:23:09 +0200

    Works..

--- src/core/save-state.js
+++ src/core/save-state.js
@@ -1,11 +1,9 @@
 import {deepCopy} from '../utils/deep-copy';
 exports.saveState = (time,state)=> {
-	console.log('save-state');
   time = time || {history: [], pos: -1};
-// delete alternate future history
+// delete future history from this point
   time.history.splice(time.pos + 1);
 // push state to history
   time.history.push(deepCopy(state));
   time.pos++;
-console.log(time);
 };
