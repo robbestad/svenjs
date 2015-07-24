3c50ae236b34af9f653a737d03e9242c8efb3b32
Sven Anders Robbestad <robbestad@gmail.com>
2015-07-24 12:06:57 +0200

    Breakthrough

--- tests/render-to-html.js
+++ tests/render-to-html.js
@@ -24,7 +24,31 @@ var div= ({tag: "div", attrs: {id:"row"}, children: [
         ]}
     ]});
 
+var createChild = (...parts) =>{
+ 	let el = document.createElement(parts[0]);
+    //console.log(parts);
+    if(parts[1]){
+    	//console.log(parts[1]);
+    	if(parts[1].hasOwnProperty('onClick')) 
+    		el.onclick = parts[1].onClick;
 
+		if(parts[1].hasOwnProperty('disabled')) 
+    		if(parts[1].disabled !== "false") el.disabled = parts[1].disabled;
+    	
+		if(parts[1].hasOwnProperty('id')){ 
+    		el.id = parts[1].id;
+    		}
+    	
+    }
+    if(parts[2]){
+    el.appendChild(document.createTextNode(parts[2]));
+    }
+    if(parts[3]){
+    el.appendChild(document.createTextNode(parts[3]));
+    }
+    return el;
+
+};
 var buildChildren = (root) =>{
     var i = 0;
     var n = 0;
@@ -36,7 +60,13 @@ var buildChildren = (root) =>{
     //if the node has children that is an array, handle it with a loop
     //&& root.children instanceof Array
       root.children.forEach((val,idx)=> {
-		docFragment.appendChild(document.createElement(val.tag));
+      	if(val.children===null)){
+      	console.log('creating children');
+      		docFragment.appendChild(createChild(val));
+      	} else {
+
+      	}
+		//docFragment.appendChild(document.createElement(val.tag));
       	// any more children?
       	console.log(val.children!==null);
 /*        if(root.children[idx] !== null) {
