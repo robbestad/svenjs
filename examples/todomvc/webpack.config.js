4386d17080183e244e1a517278754b7924e27af6
Sven A Robbestad <robbestad@gmail.com>
2015-11-25 10:04:45 +0100

    fixed dependencies

--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -7,7 +7,8 @@ var plugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   }),
-  new webpack.optimize.OccurenceOrderPlugin()
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({compress: {warnings: false},minimize: true})
 ];
 
 module.exports = {
@@ -22,7 +23,8 @@ module.exports = {
   entry: ['webpack/hot/dev-server','webpack-dev-server/client?http://localhost:3000', path.join(__dirname,'src/index.js')],
   output: {
     library: 'Svenjs',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    path: 'bundle.js'
   },
   plugins: plugins,
   resolve: {
