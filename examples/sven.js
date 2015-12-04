fc8761cc10cca977bec9ca91a1cd58beb2bfc58d
Sven A Robbestad <robbestad@gmail.com>
2015-12-04 17:29:37 +0100

    new...

--- examples/sven.js
+++ examples/sven.js
@@ -118,17 +118,34 @@ return /******/ (function(modules) { // webpackBootstrap
 	  if (spec._svenjs.rootNode) {
 	    rootNode = spec._svenjs.rootNode;
 	  };
-
-	  var sjxid = document.querySelector("[sjxid='" + spec.render().attrs.sjxid + "']");
-	  if (sjxid) {
-	    rootNode = sjxid;
-	  };
+	  if (!rootNode) rootNode = document.querySelector("[sjxid='" + spec._sjxid + "']");
+	  console.log(spec._sjxid);
 
 	  if (spec.isMounted && rootNode) {
 	    (0, _render.render)(spec, rootNode);
 	    if (spec.hasOwnProperty('_didUpdate')) spec._didUpdate.apply(spec);
 	  }
 	};
+
+	//
+	//import {render} from "../core/render";
+	//
+	//exports.lifeCycle = (spec)=> {
+	//  let rootNode;
+	//  if(spec._svenjs.rootNode){
+	//    rootNode=spec._svenjs.rootNode;
+	//  };
+	//
+	//  let sjxid=document.querySelector("[sjxid='"+spec.render().attrs.sjxid+"']");
+	//  if(sjxid){
+	//    rootNode=sjxid;
+	//  };
+	//
+	//  if(spec.isMounted && rootNode){
+	//    render(spec, rootNode);
+	//    if(spec.hasOwnProperty('_didUpdate')) spec._didUpdate.apply(spec);
+	//  }	
+	//};
 
 /***/ },
 /* 3 */
