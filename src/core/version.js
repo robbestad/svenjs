6da02936bb4f790e7d2d47a7a1b10858570aa02a
Sven A Robbestad <robbestad@gmail.com>
2015-12-06 01:09:07 +0100

    Better component handling

--- src/core/version.js
+++ src/core/version.js
@@ -1,3 +1,10 @@
+/**
+ * @module core/version
+ * @see module:svenjs
+ * @author Sven A Robbestad <sven@robbestad.com>
+ */
+'use strict';
+
 exports.version = ()=> {
-  return "0.0.3";
+  return process.env.npm_package_version;
 };
--- src/web/life-cycle.js
+++ src/web/life-cycle.js
@@ -6,30 +6,9 @@ exports.lifeCycle = (spec)=> {
     rootNode=spec._svenjs.rootNode;
   };
   if(!rootNode) rootNode=document.querySelector("[sjxid='"+spec._sjxid+"']");
-  console.log(spec._sjxid)
 
   if(spec.isMounted && rootNode){
     render(spec, rootNode);
     if(spec.hasOwnProperty('_didUpdate')) spec._didUpdate.apply(spec);
   }	
 };
-
-//
-//import {render} from "../core/render";
-//
-//exports.lifeCycle = (spec)=> {
-//  let rootNode;
-//  if(spec._svenjs.rootNode){
-//    rootNode=spec._svenjs.rootNode;
-//  };
-//
-//  let sjxid=document.querySelector("[sjxid='"+spec.render().attrs.sjxid+"']");
-//  if(sjxid){
-//    rootNode=sjxid;
-//  };
-//
-//  if(spec.isMounted && rootNode){
-//    render(spec, rootNode);
-//    if(spec.hasOwnProperty('_didUpdate')) spec._didUpdate.apply(spec);
-//  }	
-//};
